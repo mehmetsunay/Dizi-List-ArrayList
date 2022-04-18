a5ddbf8b3772e8602cb02c922f209c7db47a5d03
Mehmet Sunay <mehmetsunay@windowslive.com>
2022-04-18 16:21:24 +0300

    Dizi-List-ArrayList
    Dizi-List-ArrayList

---
+++ ConsoleList/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Dizi ile en büyük farkı boyut ve veri tipi konularındadır
+              Dizilerin boyutları belirlediğimiz kadardır ve tanımladığımız 
+              tip haricinde eleman alamazlar 
+
+             * ArrayListlerde ise örnekteki gibi durum farklıdır...
+            
+              */ 
+
+
+            ArrayList list = new ArrayList();
+            list.Add(5);
+            list.Add("MEHMET");
+
+        }
+    }
+
+
+    public class alistirmalar
+    {
+        //int[] sayilar = new int[3] {2,3,4,5};
+
+        /*List<int> sayilar = new List<int>()
+        {2,3,4,5,6};*/
+
+
+
+    }
+}
